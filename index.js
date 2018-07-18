d10548c47b60b57b2333c50969198e03f7306585
khalid jamal <kh.jamal95@gmail.com>
2018-07-18 13:23:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+  customerName=customerName.toUpperCase()
+  
+}
+
+function setBestCustomer(){
+  bestCustomer ='not bob'
+  }
